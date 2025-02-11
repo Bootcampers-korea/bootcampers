a52437219029e489a000e830eddcb90738c7d0d9
sangwoo <dev.sangwoo@gmail.com>
2025-02-11 14:50:33 +0900

    feat: Fix deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 상대 경로로 변경하여 커스텀 도메인 환경에서 정적 자원 문제 해결
+  base: '/bootcampers/', // 상대 경로로 변경하여 커스텀 도메인 환경에서 정적 자원 문제 해결
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
