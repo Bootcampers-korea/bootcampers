80734b37adae65876295787d3eef34ad2bcf7137
sangwoo <dev.sangwoo@gmail.com>
2025-02-11 09:14:12 +0900

    feat: Fix deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 상대 경로로 변경
+  base: '/bootcampers/', // GitHub 저장소 이름으로 다시 설정
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
